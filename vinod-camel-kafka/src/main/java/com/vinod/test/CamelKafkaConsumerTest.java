89927426a80234ea0ede49a7bb560a624a8d93bf
Vinod Kariyathungal Kumaran (V) <vinod_kariyathungalku@apple.com>
2016-11-01 21:52:52 -0700

    updated

--- vinod-camel-kafka/src/main/java/com/vinod/test/CamelKafkaConsumerTest.java
+++ vinod-camel-kafka/src/main/java/com/vinod/test/CamelKafkaConsumerTest.java
@@ -1,7 +1,5 @@
 
-
 package com.vinod.test;
-
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Main;
@@ -10,41 +8,51 @@ import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.kafka.KafkaConstants;
 
-
 public class CamelKafkaConsumerTest {
 
-
     public static void main(String... args) throws Exception {
-        Main main = new Main();
-        main.enableHangupSupport();
-        main.addRouteBuilder(new MyCamelJettyBuilder());
-        main.run(args);
+	Main main = new Main();
+	main.enableHangupSupport();
+	main.addRouteBuilder(new MyCamelJettyBuilder());
+	main.run(args);
     }
 }
-
 
 class MyCamelJettyBuilder extends RouteBuilder {
+    String topicName = "topic=test";
+    String kafkaServer = "kafka:localhost:9092";
+    String zooKeeperHost = "zookeeperHost=localhost&zookeeperPort=2181";
+    String serializerClass = "serializerClass=kafka.serializer.StringEncoder";
+    String autoOffsetOption = "autoOffsetReset=smallest";
+    String groupId = "groupId=testing";
+
+    String toKafka = new StringBuilder().append(kafkaServer).append("?").append(
+	    topicName).append("&").append(zooKeeperHost).append("&").append(
+		    serializerClass).toString();
+
+    String fromKafka = new StringBuilder().append(toKafka).append("&").append(
+	    autoOffsetOption).append("&").append(groupId).toString();
+
     public void configure() {
-        from("jetty:http://localhost:8182/mytestservice").process(new Processor() {
-            public void process(Exchange exchange) throws Exception {
-                String message = exchange.getIn().getBody(String.class);
-                exchange.getIn().setBody(message, String.class);
-                exchange.getIn().setHeader(KafkaConstants.PARTITION_KEY, 0);
-                exchange.getIn().setHeader(KafkaConstants.KEY, "1");
-            }
-        }).to("kafka:localhost:9092?topic=test&zookeeperHost=localhost&zookeeperPort=2181&serializerClass=kafka.serializer.StringEncoder");
-        from("kafka:localhost:9092?topic=test&zookeeperHost=localhost&zookeeperPort=2181&groupId=testing&autoOffsetReset=smallest&serializerClass=kafka.serializer.StringEncoder")
-                    .process(new Processor() {
-                        public void process(Exchange exchange) throws Exception {
-                            if (exchange.getIn() != null) {
-                                Message message = exchange.getIn();
-                                String data = message.getBody(String.class);
-                                System.out.println("Data  =" + data.toString());
-                            }
-                        }
-                    });
+	from("jetty:http://localhost:8182/mytestservice").process(
+		new Processor() {
+		    public void process(Exchange exchange) throws Exception {
+			String message = exchange.getIn().getBody(String.class);
+			exchange.getIn().setBody(message, String.class);
+			exchange.getIn().setHeader(KafkaConstants.PARTITION_KEY,
+				0);
+			exchange.getIn().setHeader(KafkaConstants.KEY, "1");
+		    }
+		}).to(toKafka);
+	from(fromKafka).process(new Processor() {
+	    public void process(Exchange exchange) throws Exception {
+		if (exchange.getIn() != null) {
+		    Message message = exchange.getIn();
+		    String data = message.getBody(String.class);
+		    System.out.println("Data  =" + data.toString());
+		}
+	    }
+	});
 
     }
 }
-
-
